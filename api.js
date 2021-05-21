91fe78081d57e153cedbf1bf3a46c83347699a4c
Luizgcsneto <luizgonzaga15@gmail.com>
2021-05-21 15:54:38 -0300

    Testando

--- api.js
+++ api.js
@@ -4,8 +4,6 @@ axios.get('https://api.github.com/users/takenet').then(function(data){
     console.log(data)
 })
 
-const api = axios.create({
-    baseUrl: 'https://api.github.com'
-});
+
 
 module.exports = api;
